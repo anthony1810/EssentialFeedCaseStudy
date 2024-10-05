836ab11e56d23b4c7813776cffa5866d38dfa571
Anthony <quang.tran@appetiser.com.au>
2024-10-05 09:07:01 +0700

    Add initial Remote Loader Tests

---
+++ EssentialFeed/EssentialFeedTests/RemoteLoaderTests.swift
@@ -0,0 +1,15 @@
+//
+//  RemoteLoaderTests.swift
+//  EssentialFeed
+//
+//  Created by Anthony on 5/10/24.
+//
+
+import XCTest
+@testable import EssentialFeed
+
+class RemoteLoaderTests: XCTestCase {
+    func test_init_doesNotRequestDataFromURL() {
+        
+    }
+}
