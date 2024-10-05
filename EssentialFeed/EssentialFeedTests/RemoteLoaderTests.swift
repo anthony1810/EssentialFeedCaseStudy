3cc25fe2789e91b7389ec9116f8fbe331154ae1f
Anthony <quang.tran@appetiser.com.au>
2024-10-05 09:42:37 +0700

    add initial test for httpclient spy

--- EssentialFeed/EssentialFeedTests/RemoteLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteLoaderTests.swift
@@ -8,8 +8,62 @@
 import XCTest
 @testable import EssentialFeed
 
+class RemoteFeedLoader: FeedLoader {
+    let httpClient: HTTPClient
+    let url: URL
+    
+    init(httpClient: HTTPClient, url: URL) {
+        self.httpClient = httpClient
+        self.url = url
+    }
+    
+    func load(completion: @escaping (EssentialFeed.LoadFeedResult) -> Void) {
+        httpClient.get(url: self.url)
+    }
+}
+
+protocol HTTPClient {
+    var requestedURL: URL? { get }
+    func get(url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
+    var requestedURL: URL?
+    
+    init(requestedURL: URL? = nil) {
+        self.requestedURL = requestedURL
+    }
+    
+    func get(url: URL) {
+        requestedURL = url
+    }
+}
+
+
 class RemoteLoaderTests: XCTestCase {
     func test_init_doesNotRequestDataFromURL() {
         
+        // Given (Arrange)
+        let url = URL(string: "https:/a-url.com")!
+        let (client, _) = makeSUT(url: url, client: HTTPClientSpy())
+        
+        // Then
+        XCTAssertEqual(client.requestedURL, nil)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https:/a-url.com")!
+        let (client, sut) = makeSUT(url: url, client: HTTPClientSpy())
+        
+        sut.load(completion: {_ in })
+        
+        XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    private func makeSUT(url: URL, client: HTTPClient) -> (HTTPClient, FeedLoader) {
+        let client = HTTPClientSpy(requestedURL: nil)
+        let sut = RemoteFeedLoader(httpClient: client, url: url)
+        
+        return (client, sut)
     }
 }
