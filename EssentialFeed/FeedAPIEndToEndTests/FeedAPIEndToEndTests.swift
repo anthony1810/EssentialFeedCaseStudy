7c5f29e237235a07f403bbf97197645fabea643f
Anthony <qquang269@gmail.com>
2024-10-10 09:23:40 +0700

    add tracking memory leak for httpClient and remote feed loader

--- EssentialFeed/FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
+++ EssentialFeed/FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
@@ -18,10 +18,13 @@ final class FeedAPIEndToEndTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func test_endToEndFeedResult_matchesFixedTestSampleData() throws {
+    func test_endToEndFeedResult_matchesFixedTestSampleData(file: StaticString = #file, line: UInt = #line) throws {
         let url = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let httpclient = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(httpClient: httpclient, url: url)
+        
+        trackForMemoryLeaks(httpclient, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
         
         var capturedResult: LoadFeedResult?
         let expectation = self.expectation(description: "Loaded")
