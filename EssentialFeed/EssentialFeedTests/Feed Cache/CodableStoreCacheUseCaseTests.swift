0899cfd78d1c414376095454ac7bb781ad131c1f
Anthony <qquang269@gmail.com>
2024-10-19 21:31:26 +0700

    insert and then retrieve return expected value from `CodableFeedStore`

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableStoreCacheUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableStoreCacheUseCaseTests.swift
@@ -10,14 +10,67 @@ import XCTest
 import EssentialFeed
 
 class CodableFeedStore {
+    
+    let storeURL: URL
+    
+    init(storeURL: URL) {
+        self.storeURL = storeURL
+    }
+    
+    struct Cache: Codable {
+        let items: [LocalFeedImage]
+        let timestamp: Date
+    }
+    
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
-        completion(.empty)
+        do {
+            guard let data = try? Data(contentsOf: storeURL) else {
+                return completion(.empty)
+            }
+            let cache = try JSONDecoder().decode(Cache.self, from: data)
+            completion(.success(cache.items, cache.timestamp))
+        } catch {
+            completion(.failure(error))
+        }
+       
+    }
+    
+    func insertCache(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCacheCompletion) {
+        
+        let encoder = JSONEncoder()
+        let cache = Cache(items: items, timestamp: timestamp)
+        let encoded = try! encoder.encode(cache)
+        try! encoded.write(to: storeURL)
+        
+        completion(nil)
     }
 }
 
-class CodableStoreCacheUseCaseTests: XCTestCase {
+class CodableStoreCacheUseCaseTests: FeedCacheTests {
+    
+    override func setUp() {
+        super.setUp()
+        
+        setUpState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        clearUpState()
+    }
+    
+    func setUpState() {
+        try? FileManager.default.removeItem(at: storeURL)
+    }
+    
+    func clearUpState() {
+        try? FileManager.default.removeItem(at: storeURL)
+    }
+    
     func test_retrieve_deliversEmptyOnEmptyCache() {
-        let sut = CodableFeedStore()
+       
+        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for cache retrieval")
         var capturedResult: RetrievalResult?
@@ -35,7 +88,7 @@ class CodableStoreCacheUseCaseTests: XCTestCase {
     }
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
-        let sut = CodableFeedStore()
+        let sut = makeSUT()
         
         let exp = expectation(description: "Wait for cache retrieval")
         var firstCapturedResult: RetrievalResult?
@@ -54,5 +107,45 @@ class CodableStoreCacheUseCaseTests: XCTestCase {
         case (.empty, .empty): break
         default: XCTFail("expected empty cache, got different result)")
         }
+    }
+    
+    func test_retrieve_insertThenRetrieveExpectedvalue() {
+        let sut = makeSUT()
+        let expectedItem = uniqueItem().localModel
+        let timeStamp = Date()
+        
+        var capturedInsertedError: Error?
+        var capturedRetrievedResult: RetrievalResult?
+        let exp = expectation(description: "Wait for cache retrieval")
+      
+        sut.insertCache([expectedItem], timestamp: timeStamp) { insertedError in
+            capturedInsertedError = insertedError
+            sut.retrieve { result in
+                capturedRetrievedResult = result
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertNil(capturedInsertedError)
+        
+        switch capturedRetrievedResult {
+        case .success(let feedItems,  _):
+            XCTAssertEqual(feedItems, [expectedItem])
+        default: XCTFail("expected success, got different result)")
+        }
+    }
+}
+
+// MARK: - Helpers
+extension CodableStoreCacheUseCaseTests {
+    func makeSUT(file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
+        let sut = CodableFeedStore(storeURL: storeURL)
+        return sut
+    }
+    
+    var storeURL: URL {
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self))")
     }
 }
