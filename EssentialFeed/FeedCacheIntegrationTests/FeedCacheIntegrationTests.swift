c16326927e331b8ddb732339f0e44d17a484ebdc
Anthony <quang.tran@appetiser.com.au>
2024-10-24 10:07:01 +0700

    guarantee that `RealmFeedStore` on `LocalFeedLoader` integration delivers empty feeds on init

--- EssentialFeed/FeedCacheIntegrationTests/FeedCacheIntegrationTests.swift
+++ EssentialFeed/FeedCacheIntegrationTests/FeedCacheIntegrationTests.swift
@@ -6,30 +6,50 @@
 //
 
 import XCTest
+import EssentialFeed
+import RealmSwift
 
 final class FeedCacheIntegrationTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override class func setUp() {
+        super.setUp()
+        
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        super.tearDown()
+        
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func test_load_deliversNoItemsOnEmptyCache() throws {
+        let sut = makeSUT()
+        let exp = expectation(description: "Wait for load completion")
+        
+        var actualResult: LoadFeedResult?
+        sut.load { result in
+            actualResult = result
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        switch actualResult {
+        case .success(let feeds):
+            XCTAssertEqual(feeds, [])
+        case .failure(let error):
+            XCTFail("Expected success, got \(error)")
+        default:
+            XCTFail("Expected success or failure, got nothing")
         }
     }
 
+}
+
+extension FeedCacheIntegrationTests {
+    private func makeSUT() -> FeedLoader {
+        let cacheStore = RealmFeedStore()
+        let feedloader = LocalFeedLoader(store: cacheStore, timestamp: Date.init)
+        
+        return feedloader
+    }
 }
