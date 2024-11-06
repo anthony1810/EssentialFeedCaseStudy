9410f89b6bbf858b2f4e09ad41a110a100c66ebe
Quang Tran <qquang269@gmail.com>
2024-11-06 16:50:39 +0700

    create makeSUT function

--- EssentialFeed/EssentialFeedTests/FeedImagePresenter/FeedImagePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedImagePresenter/FeedImagePresenterTests.swift
@@ -18,9 +18,12 @@ final class FeedImagePresenter {
 final class FeedImagePresenterTests: XCTestCase {
     
     func test_init_doesNotSendAnyMessage() {
-        let view = ViewSpy()
-        _ = FeedImagePresenter(view: view)
+       let (_, view) = makeSUT()
         XCTAssertEqual(view.messages.count, 0)
+    }
+    
+    func test_startLoadingImage_showLoadingAndDoesNotShowRetry() {
+        
     }
     
 }
@@ -28,5 +31,15 @@ final class FeedImagePresenterTests: XCTestCase {
 extension FeedImagePresenterTests {
     private final class ViewSpy {
         var messages: [Any] = []
+    }
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedImagePresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedImagePresenter(view: view)
+        
+        trackForMemoryLeaks(view)
+        trackForMemoryLeaks(sut)
+        
+        return (sut, view)
     }
 }
