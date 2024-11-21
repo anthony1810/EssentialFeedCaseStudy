5784ce2fecac6c7d9b9455e1a29e7a96037d1ce8
Quang Tran <qquang269@gmail.com>
2024-11-21 12:44:02 +0700

    `FeedImageLoaderWithFallbackComposite.loadImageData` loads from fallback when primary fails

--- EssentialApp/EssentialAppTests/FeedImageLoaderWithFallbackCompositetests.swift
+++ EssentialApp/EssentialAppTests/FeedImageLoaderWithFallbackCompositetests.swift
@@ -26,7 +26,14 @@ class FeedImageDataLoaderWithFallbackComposite: FeedImageLoaderProtocol {
     }
     
     func loadImageData(from url: URL, completion: @escaping (FeedImageLoaderProtocol.Result) -> Void) -> ImageLoadingDataTaskProtocol {
-        _ = primary.loadImageData(from: url, completion: completion)
+        _ = primary.loadImageData(from: url, completion: { [weak self] result in
+            switch result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure:
+                _ = self?.fallback.loadImageData(from: url, completion: completion)
+            }
+        })
         return Task()
     }
 }
@@ -50,6 +57,17 @@ class FeedImageLoaderWithFallbackCompositetests: XCTestCase {
         XCTAssertEqual(primary.requestedURLs, [expectedImageURL])
         XCTAssertTrue(fallback.requestedURLs.isEmpty)
     }
+    
+    func test_loadImageData_loadsFromFallBackOnPrimaryFails() {
+        let expectedImageURL = makeAnyUrl()
+        let expectedData = makeAnyData()
+        let (sut, primary, fallback) = makeSUT()
+        
+        expect(sut: sut, toLoad: expectedImageURL, with: .success(expectedData)) {
+            primary.completeLoad(with: .failure(makeAnyError()))
+            fallback.completeLoad(with: .success(expectedData))
+        }
+    }
 }
 
 extension FeedImageLoaderWithFallbackCompositetests {
@@ -64,6 +82,25 @@ extension FeedImageLoaderWithFallbackCompositetests {
         trackForMemoryLeaks(fallback, file: file, line: line)
         
         return (sut, primary, fallback)
+    }
+    
+    func expect(sut: FeedImageDataLoaderWithFallbackComposite, toLoad url: URL, with expectedResult: FeedImageLoaderProtocol.Result, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load")
+        _ = sut.loadImageData(from: url, completion: { actualResult in
+            switch (actualResult, expectedResult) {
+            case let (.success(actualData), .success(expectedData)):
+                XCTAssertEqual(actualData, expectedData, file: file, line: line)
+            case let (.failure(actualError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(actualError, expectedError)
+            default:
+                XCTFail("Expect \(expectedResult), but got \(actualResult) instead")
+            }
+            exp.fulfill()
+        })
+        
+        action()
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     private class LoaderSpy: FeedImageLoaderProtocol {
@@ -82,6 +119,10 @@ extension FeedImageLoaderWithFallbackCompositetests {
             messages.append((url, completion))
             
             return Task()
+        }
+        
+        func completeLoad(with result: FeedImageLoaderProtocol.Result, at index: Int = 0) {
+            messages[index].completion(result)
         }
     }
 }
