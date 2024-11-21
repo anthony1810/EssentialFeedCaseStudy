ea1f9e6726017241bcb406ba3408f86c9ed4dd40
Quang Tran <qquang269@gmail.com>
2024-11-21 09:07:53 +0700

    Remove unused test case

---
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,16 @@
+//
+//  FeedLoaderWithFallbackCompositeTests.swift
+//  EssentialApp
+//
+//  Created by Anthony on 21/11/24.
+//
+import Foundation
+import XCTest
+import EssentialFeed
+
+class FeedLoaderWithFallbackCompositeTests: XCTestCase {
+    
+    func test_loadFeed_deliversSuccessWhenPrimarySuccess() {
+        
+    }
+}
