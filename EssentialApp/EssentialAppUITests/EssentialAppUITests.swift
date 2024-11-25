8b7edf5e57a927c034da9d8d88888de217fe7214
Quang Tran <qquang269@gmail.com>
2024-11-25 16:13:53 +0700

    on app launch displays remote feed when customer has connectivity

--- EssentialApp/EssentialAppUITests/EssentialAppUITests.swift
+++ EssentialApp/EssentialAppUITests/EssentialAppUITests.swift
@@ -10,9 +10,21 @@ import XCTest
 final class EssentialAppUITests: XCTestCase {
     
     @MainActor
-    func testExample() throws {
+    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() throws {
         let app = XCUIApplication()
         app.launch()
+        
+        let feedCells = app.cells.matching(identifier: "feed-image-cell")
+
+        // Wait for at least one feed cell to appear
+        let firstCell = feedCells.element(boundBy: 0)
+        XCTAssertTrue(firstCell.waitForExistence(timeout: 5), "No feed cells appeared within the timeout.")
+
+        // Now assert the count if needed
+        XCTAssertEqual(feedCells.count, 22, "Unexpected number of feed cells.")
+        
+        let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch.waitForExistence(timeout: 10.0)
+        XCTAssertTrue(firstImage)
     }
     
 }
