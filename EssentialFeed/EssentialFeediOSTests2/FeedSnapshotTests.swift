7fea19ae35daca4143c1f889d2371709a510d9a6
Quang Tran <qquang269@gmail.com>
2024-11-30 09:48:47 +0700

    Record feed with content snapshot

--- EssentialFeed/EssentialFeediOSTests2/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests2/FeedSnapshotTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-import EssentialFeed
+@testable import EssentialFeed
 import EssentialFeediOS
 
 final class FeedSnapshotTests: XCTestCase {
@@ -16,6 +16,14 @@ final class FeedSnapshotTests: XCTestCase {
         sut.display(emptyFeed())
 
         record(snapshot: sut.snapshot(), named: "EMPTY_FEED")
+    }
+    
+    func test_feedWithImages() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithContents())
+        
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
     }
 }
 
@@ -34,6 +42,21 @@ extension FeedSnapshotTests {
     
     private func emptyFeed() -> [FeedImageCellController] {
         []
+    }
+    
+    private func feedWithContents() -> [FeedStub] {
+        [
+            FeedStub(
+                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .red), url: URL(string: "http://anyurl.com")!
+            ),
+            FeedStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .green), url: URL(string: "http://anyurl.com")!
+            )
+        ]
     }
     
     private func record(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
@@ -56,9 +79,37 @@ extension FeedSnapshotTests {
     }
 }
 
+class FeedStub: FeedImageDataControllerDelegate {
+    let viewModel: FeedImageViewModel<UIImage>
+    weak var controller: FeedImageCellController?
+    
+    init(description: String?, location: String?, image: UIImage?, url: URL) {
+        viewModel = FeedImageViewModel(location: location, description: description, url: url, image: image, isLoading: false, shouldRetry: image == nil)
+    }
+    
+    func didRequestImage() {
+        controller?.display(viewModel)
+    }
+    
+    func didCancelImageRequest() {
+        
+    }
+}
+
 extension FeedViewController {
     func display(_ cells: [FeedImageCellController]) {
         self.tableModels = cells
+    }
+    
+    func display(_ stubs: [FeedStub]) {
+        let cells: [FeedImageCellController] = stubs.map { stub in
+            let cellController = FeedImageCellController(delegate: stub)
+            stub.controller = cellController
+            
+            return cellController
+        }
+        
+        display(cells)
     }
 }
 
@@ -70,3 +121,16 @@ extension UIViewController {
         }
     }
 }
+
+extension UIImage {
+     static func make(withColor color: UIColor) -> UIImage {
+         let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+         UIGraphicsBeginImageContext(rect.size)
+         let context = UIGraphicsGetCurrentContext()!
+         context.setFillColor(color.cgColor)
+         context.fill(rect)
+         let img = UIGraphicsGetImageFromCurrentImageContext()
+         UIGraphicsEndImageContext()
+         return img!
+     }
+ }
