cddc6f9d1eb97d2ee48b870ddf10323020f9a423
Quang Tran <qquang269@gmail.com>
2024-12-02 10:54:00 +0700

    check `isMainQueue` instead of just `isMainThread` since there's no guarantee that the main thread is running on main queue

--- EssentialApp/EssentialApp/Composers/Helpers/MainThreadDecorator/Publisher+ImmediateWhenOnMainQueue.swift
+++ EssentialApp/EssentialApp/Composers/Helpers/MainThreadDecorator/Publisher+ImmediateWhenOnMainQueue.swift
@@ -23,7 +23,7 @@ extension Publisher where Output == Data? {
 
 extension DispatchQueue {
     static var ImmediateWhenOnMainQueue: ImmediateWhenOnMainQueueScheduler {
-        ImmediateWhenOnMainQueueScheduler()
+        ImmediateWhenOnMainQueueScheduler.shared
     }
     
     struct ImmediateWhenOnMainQueueScheduler: Scheduler {
@@ -31,6 +31,15 @@ extension DispatchQueue {
         typealias SchedulerTimeType = DispatchQueue.SchedulerTimeType
 
         typealias SchedulerOptions = DispatchQueue.SchedulerOptions
+        
+        static let shared = Self()
+        
+        private static let key = DispatchSpecificKey<UInt8>()
+        private static let value = UInt8.max
+        
+        private init() {
+            DispatchQueue.main.setSpecific(key: Self.key, value: Self.value)
+        }
 
         var now: Self.SchedulerTimeType {
             DispatchQueue.main.now
@@ -39,9 +48,13 @@ extension DispatchQueue {
         var minimumTolerance: Self.SchedulerTimeType.Stride {
             DispatchQueue.main.minimumTolerance
         }
+        
+        private var isMainQueue: Bool {
+            DispatchQueue.getSpecific(key: Self.key) == Self.value
+        }
 
         func schedule(options: Self.SchedulerOptions?, _ action: @escaping () -> Void) {
-            guard Thread.isMainThread else {
+            guard isMainQueue else {
                 return DispatchQueue.main.schedule(options: options, action)
             }
             
