6d73e01b655a3af0e34a5cc5dd6956fbf0206a66
Quang Tran <qquang269@gmail.com>
2024-12-04 09:39:01 +0700

    remove unused array extension on `RemoteImageCommentLoader`

--- EssentialFeed/EssentialFeed/ImageCommentAPI/RemoteImageCommentsLoader.swift
+++ EssentialFeed/EssentialFeed/ImageCommentAPI/RemoteImageCommentsLoader.swift
@@ -45,11 +45,3 @@ public final class RemoteImageCommentsLoader {
         }
     }
 }
-
-extension Array where Element == RemoteFeedItem {
-    func toFeedComments() -> [FeedImage] {
-        map {
-            FeedImage(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image)
-        }
-    }
-}
