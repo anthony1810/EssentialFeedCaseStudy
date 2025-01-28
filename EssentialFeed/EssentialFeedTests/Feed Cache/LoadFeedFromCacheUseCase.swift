0f113861fb527b741f2123866d0dc69432730612
Anthony Tran <qquang269@gmail.com>
2025-01-28 10:53:54 +0700

    save commands requests cache insertion with timestamp on success deletion

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCase.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCase.swift
@@ -9,33 +9,55 @@ import XCTest
 import EssentialFeed
 
 final class FeedStore {
-    var deletionCacheCount: Int = 0
-    var insertionCacheCount: Int = 0
-    
-    func deleteCachedFeed() {
-        deletionCacheCount += 1
+    struct InsertionMessage {
+        let items: [FeedItem]
+        let timestamp: Date
     }
     
-    func insertCachedFeed(_ items: [FeedItem]) {
-        insertionCacheCount += 1
+    typealias DeletionCompletion = (Result<Void, Error>) -> Void
+    typealias InsertionCompletion = (Result<[InsertionMessage], Error>) -> Void
+    
+    var deletionCacheCount: Int {
+        deletionCompletions.count
+    }
+    var deletionCompletions = [DeletionCompletion]()
+    
+    var insertionCacheCount: Int {
+        insertions.count
+    }
+    var insertions: [InsertionMessage] = []
+    
+    
+    func deleteCachedFeed(completion: @escaping (Result<Void, Error>) -> Void) {
+        deletionCompletions.append(completion)
+    }
+    
+    func insertCachedFeed(_ items: [FeedItem], timestamp: Date) {
+        insertions.append(.init(items: items, timestamp: timestamp))
     }
     
     func completeDeletion(with result: Result<Void, Error>, at index: Int = 0) {
-        if case .success = result {
-            insertionCacheCount += 1
-        }
+        deletionCompletions[index](result)
     }
 }
 
 final class LocalFeedLoader {
     let store: FeedStore
+    let currentDate: () -> Date
     
-    init(store: FeedStore) {
+    init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
+        self.currentDate = currentDate
     }
     
     func save(_ items: [FeedItem]) {
-        store.deleteCachedFeed()
+        store.deleteCachedFeed(completion: { [weak self] result in
+            guard let self else { return }
+            
+            if case .success = result {
+                self.store.insertCachedFeed(items, timestamp: self.currentDate())
+            }
+        })
     }
 }
 
@@ -75,10 +97,27 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.insertionCacheCount, 1)
     }
     
+    func test_save_requestNewCacheInsertionWithTimestampOnSuccessfullyDeletion() {
+        let currentDate = Date()
+        let (sut, store) = makeSUT(currentDate: { currentDate })
+        
+        let feeds = [uniqueFeed(), uniqueFeed()]
+        sut.save(feeds)
+        store.completeDeletion(with: .success(()))
+        
+        XCTAssertEqual(store.insertionCacheCount, 1)
+        XCTAssertEqual(store.insertions.first?.items, feeds)
+        XCTAssertEqual(store.insertions.first?.timestamp, currentDate)
+    }
+    
     // MARK: - Helper
-    func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
+    func makeSUT(
+        currentDate: @escaping () -> Date = Date.init,
+        file: StaticString = #filePath,
+        line: UInt = #line)
+    -> (sut: LocalFeedLoader, store: FeedStore) {
         let store = FeedStore()
-        let loader = LocalFeedLoader(store: store)
+        let loader = LocalFeedLoader(store: store, currentDate: currentDate)
         
         trackMemoryLeaks(store, file: file, line: line)
         trackMemoryLeaks(loader, file: file, line: line)
