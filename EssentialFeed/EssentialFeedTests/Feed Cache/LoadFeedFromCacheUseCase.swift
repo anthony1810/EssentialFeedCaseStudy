f9fd56a9794bbb5a75b1b35cc0cdde1b2e5bbc50
Anthony Tran <qquang269@gmail.com>
2025-01-28 09:56:23 +0700

    `LocalFeedLoader` does not request deletion when init

--- EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCase.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCase.swift
@@ -8,8 +8,25 @@
 import XCTest
 import EssentialFeed
 
+final class FeedStore {
+    var deletionCacheCount: Int = 0
+}
+
+final class LocalFeedLoader {
+    let store: FeedStore
+    
+    init(store: FeedStore) {
+        self.store = store
+    }
+    
+    func load() {}
+}
+
 final class LoadFeedFromCacheUseCaseTests: XCTestCase {
-    func testExample() throws {
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
         
+        XCTAssertEqual(store.deletionCacheCount, 0)
     }
 }
