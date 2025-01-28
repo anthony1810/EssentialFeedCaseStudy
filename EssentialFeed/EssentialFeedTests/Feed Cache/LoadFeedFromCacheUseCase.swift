1aa556c7451a332e945f28abdd46fe301144df0b
Anthony Tran <qquang269@gmail.com>
2025-01-28 09:46:30 +0700

    Rename and add file in preparation for testing cache flow

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCase.swift
@@ -0,0 +1,15 @@
+//
+//  LoadFeedFromCacheUseCase.swift
+//  EssentialFeed
+//
+//  Created by Anthony on 28/1/25.
+//
+
+import XCTest
+import EssentialFeed
+
+final class LoadFeedFromCacheUseCaseTests: XCTestCase {
+    func testExample() throws {
+        
+    }
+}
