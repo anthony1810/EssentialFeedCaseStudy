db3788b56c3ccc5293fa7d1b773eb0826db4acad
Anthony Tran <qquang269@gmail.com>
2025-02-04 20:28:41 +0700

    prevent leaking infrastructure details `Codable` to `LocalFeedImage` struct which is a domain details

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -20,15 +20,43 @@ final class CodableFeedStore {
     }
     
     private struct Cache: Codable {
-        let items: [LocalFeedImage]
+        let items: [CodableFeedImage]
         let timestamp: Date
+        var localFeedImages: [LocalFeedImage] {
+            items.map(\.localFeedImage)
+        }
+    }
+    
+    private struct CodableFeedImage: Codable {
+        public let id: UUID
+        public let description: String?
+        public let location: String?
+        public let url: URL
+        
+        public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+            self.id = id
+            self.description = description
+            self.location = location
+            self.url = imageURL
+        }
+        
+        public init(from localFeedImage: LocalFeedImage) {
+            self.id = localFeedImage.id
+            self.description = localFeedImage.description
+            self.location = localFeedImage.location
+            self.url = localFeedImage.url
+        }
+        
+        public var localFeedImage: LocalFeedImage {
+            LocalFeedImage(id: id, description: description, location: location, imageURL: url)
+        }
     }
     
     func retrievalCachedFeed(completion: @escaping FeedStore.RetrievalCompletion) {
         do {
             if let encoded = try? Data(contentsOf: storeUrl) {
                 let decoded = try decoder.decode(Cache.self, from: encoded)
-                completion(.found(feed: decoded.items, timestamp: decoded.timestamp))
+                completion(.found(feed: decoded.localFeedImages, timestamp: decoded.timestamp))
             } else {
                 completion(.empty)
             }
@@ -38,7 +66,7 @@ final class CodableFeedStore {
     }
     
     func insertCachedFeed(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
-        let cache = Cache(items: items, timestamp: timestamp)
+        let cache = Cache(items: items.map(CodableFeedImage.init), timestamp: timestamp)
         let encoded = try! encoder.encode(cache)
         try! encoded.write(to: storeUrl)
         
