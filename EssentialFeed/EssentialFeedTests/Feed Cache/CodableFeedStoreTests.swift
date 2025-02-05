1cf014aabde821a65d73efd609e45c24c444fd73
Anthony Tran <qquang269@gmail.com>
2025-02-05 19:36:49 +0700

    refactor with expect helper function when retrieve from cache

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -25,17 +25,8 @@ final class CodableFeedStoreTests: XCTestCase {
     
     func test_retrieve_deliversEmptyCacheOnEmptyCache() {
         let sut = makeSUT()
-        let exp = expectation(description: "Waitng for completion")
         
-        sut.retrievalCachedFeed { result in
-            switch result {
-            case .empty: break
-            default: XCTFail("Expect empty cache got \(result)")
-            }
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toReceive: .empty)
     }
     
     func test_retrieveTwice_deliversSameEmptyCacheOnEmptyCache() {
@@ -112,6 +103,26 @@ final class CodableFeedStoreTests: XCTestCase {
         trackMemoryLeaks(sut, file: file, line: line)
         
         return sut
+    }
+    
+    func expect(_ sut: CodableFeedStore, toReceive expectedResult: RetrieveCacheFeedResult, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "Waiting for completion")
+        
+        sut.retrievalCachedFeed { result in
+            switch (result, expectedResult) {
+            case (.empty, .empty): break
+            case let (.found(receivedItems, receivedTimestamp), .found(expectedItems, expectedTimestamp)):
+                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
+                XCTAssertEqual(receivedTimestamp, expectedTimestamp, file: file, line: line)
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError as NSError?, expectedError as NSError?, file: file, line: line)
+            default: XCTFail("Expect \(expectedResult) got \(result)", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     private func deleteStoreArtifacts() {
