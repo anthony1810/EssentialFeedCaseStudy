b3a0385938af2673768a14b598e349ee17c8021c
Anthony Tran <qquang269@gmail.com>
2025-02-07 10:27:20 +0700

    assert that `CoreDataFeedStore` does not have side effect on retrieve empty cache twice

---
+++ EssentialFeed/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -0,0 +1,24 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Anthony on 7/2/25.
+//
+
+import Foundation
+
+public final class CoreDataFeedStore: FeedStore {
+    public init() {}
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    public func insertCachedFeed(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    public func retrievalCachedFeed(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+}
--- EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -11,11 +11,15 @@ import EssentialFeed
 
 final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     func test_retrieve_deliversEmptyCacheOnEmptyCache() throws {
+        let sut = try makeSUT()
         
+        assertThatRetrieveDeliversEmptyCacheOnEmptyCache(on: sut)
     }
     
     func test_retrieveTwice_deliversSameEmptyCacheOnEmptyCache() throws {
+        let sut = try makeSUT()
         
+        assertThatRetrieveHasNoSideEffectOnDeliversEmptyCache(on: sut)
     }
     
     func test_retrieve_deliversNonEmptyCacheOnNonEmptyCache() throws {
@@ -44,5 +48,13 @@ final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     func test_storeSideEffects_runSerially() throws {
         
+    }
+    
+    // MARK: - Helpers
+    func makeSUT(file: StaticString = #file, line: UInt = #line) throws -> FeedStore {
+        let sut = CoreDataFeedStore()
+        trackMemoryLeaks(sut, file: file, line: line)
+        
+        return sut
     }
 }
