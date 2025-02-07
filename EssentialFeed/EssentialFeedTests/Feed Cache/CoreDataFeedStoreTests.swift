656c27caea2bcb1449e6900057f0ea8343e13ee8
Anthony Tran <qquang269@gmail.com>
2025-02-07 10:04:47 +0700

    add empty Codata Feed store specs

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,48 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeed
+//
+//  Created by Anthony on 7/2/25.
+//
+
+import Foundation
+import XCTest
+import EssentialFeed
+
+final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyCacheOnEmptyCache() throws {
+        
+    }
+    
+    func test_retrieveTwice_deliversSameEmptyCacheOnEmptyCache() throws {
+        
+    }
+    
+    func test_retrieve_deliversNonEmptyCacheOnNonEmptyCache() throws {
+        
+    }
+    
+    func test_retrieve_deliversFoundCacheHasNoSideEffects() throws {
+        
+    }
+    
+    func test_insert_overridesExistingCacheOnNonEmptyCache() throws {
+        
+    }
+    
+    func test_insert_overridesExistingCacheOnNonEmptyCacheHasNoSideEffect() throws {
+        
+    }
+    
+    func test_delete_deliversSuccessOnEmptyCache() throws {
+        
+    }
+    
+    func test_delete_deliversSuccessOnNonEmptyCache() throws {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() throws {
+        
+    }
+}
