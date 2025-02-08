65cba455d0f6cd949ad27ac665429aceff815294
Anthony Tran <qquang269@gmail.com>
2025-02-08 10:55:23 +0700

    extract context.perform to a helper function

--- EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -18,8 +18,7 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        let context = self.context
-        context.perform {
+        perform { context in
             do {
                 try ManagedCache.find(in: context).map(context.delete).map(context.save)
                 completion(nil)
@@ -30,8 +29,7 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func insertCachedFeed(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        let context = self.context
-        context.perform {
+        perform { context in
             do {
                 let managedCache = try ManagedCache.newUniqueInstance(in: context)
                 managedCache.timestamp = timestamp
@@ -47,8 +45,7 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func retrievalCachedFeed(completion: @escaping RetrievalCompletion) {
-        let context = self.context
-        context.perform {
+        perform { context in
             do {
                 if let cache = try ManagedCache.find(in: context) {
                     completion(.found(feed: cache.localFeeds, timestamp: cache.timestamp))
@@ -58,6 +55,12 @@ public final class CoreDataFeedStore: FeedStore {
             } catch {
                 completion(.failure(error))
             }
+        }
+    }
+    
+    private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+        context.perform { [context] in
+            action(context)
         }
     }
 }
