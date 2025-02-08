22e398079474111d88137052121643011d4b7d3e
Anthony Tran <qquang269@gmail.com>
2025-02-08 21:00:41 +0700

    Add new target for feed cache integration test

---
+++ EssentialFeed/EssentialFeedCacheInterationTests/EssentialFeedCacheInterationTests.swift
@@ -0,0 +1,12 @@
+//
+//  EssentialFeedCacheInterationTests.swift
+//  EssentialFeedCacheInterationTests
+//
+//  Created by Anthony on 8/2/25.
+//
+
+import XCTest
+
+final class EssentialFeedCacheInterationTests: XCTestCase {
+    
+}
