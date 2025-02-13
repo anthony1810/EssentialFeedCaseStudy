0f5fb48ce01992e819bdbbf9d2b2567926a27325
Anthony Tran <qquang269@gmail.com>
2025-02-13 20:51:20 +0700

    add `FeedViewController`

---
+++ EssentialFeed/EssentialFeedPrototye/FeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FeedViewController.swift
+//  EssentialFeed
+//
+//  Created by Anthony on 13/2/25.
+//
+
+import UIKit
+
+final class FeedViewController: UITableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedViewCell", for: indexPath)
+        
+        return cell
+    }
+}
