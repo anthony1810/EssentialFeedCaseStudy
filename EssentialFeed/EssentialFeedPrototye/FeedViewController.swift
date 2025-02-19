a75c0dfe1f2f95661d611375a82864671f6afca6
Anthony Tran <qquang269@gmail.com>
2025-02-19 21:21:55 +0700

    simulate loading and refresh action on prototype

--- EssentialFeed/EssentialFeedPrototye/FeedViewController.swift
+++ EssentialFeed/EssentialFeedPrototye/FeedViewController.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 final class FeedViewController: UITableViewController {
-    let feed: [FeedImageViewModel] = FeedImageViewModel.prototypeFeed
+    var feed: [FeedImageViewModel] = []
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        refresh()
+    }
+    
+    @IBAction func refresh() {
+        self.refreshControl?.beginRefreshing()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) { [unowned self] in
+            if self.feed.isEmpty {
+                self.feed = FeedImageViewModel.prototypeFeed
+                self.tableView.reloadData()
+            }
+            self.refreshControl?.endRefreshing()
+        }
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         feed.count
