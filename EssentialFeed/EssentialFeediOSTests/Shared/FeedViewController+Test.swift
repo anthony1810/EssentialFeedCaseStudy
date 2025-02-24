af01e34149c6db1527fcd02f2f344b7714b4d699
Anthony Tran <qquang269@gmail.com>
2025-02-24 21:03:25 +0700

    extract `FakeUIRefreshControl`

--- EssentialFeed/EssentialFeediOSTests/Shared/FeedViewController+Test.swift
+++ EssentialFeed/EssentialFeediOSTests/Shared/FeedViewController+Test.swift
@@ -103,18 +103,4 @@ extension FeedViewController {
         
         refreshControl = fakeRefreshControl
     }
-    
-    private class FakeUIRefreshControl: UIRefreshControl {
-        private var _isRefreshing = false
-        
-        override var isRefreshing: Bool { _isRefreshing }
-        
-        override func beginRefreshing() {
-            _isRefreshing = true
-        }
-        
-        override func endRefreshing() {
-            _isRefreshing = false
-        }
-    }
 }
