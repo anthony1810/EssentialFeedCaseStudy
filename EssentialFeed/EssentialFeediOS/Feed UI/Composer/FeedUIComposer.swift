422fbef879e412128f2b668e2939adb954a3b5ee
Anthony Tran <qquang269@gmail.com>
2025-03-09 19:50:43 +0700

    move `FeedImage` value translation to `FeedImageViewModel`, now `FeedImageCellController` act as a binder between `FeedImageCell` and `FeedImageViewModel`

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composer/FeedUIComposer.swift
@@ -0,0 +1,24 @@
+import EssentialFeed
+import UIKit
+
+public final class FeedUIComposer {
+    private init() {}
+    
+    public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
+        let feedViewModel = FeedViewModel(feedLoader: feedLoader)
+        let refreshController = FeedRefreshViewController(viewModel: feedViewModel)
+        let feedController = FeedViewController(refreshController: refreshController)
+        
+        feedViewModel.onFeedLoad = adaptFeedCellControllers(forwardingTo: feedController, loader: imageLoader)
+        
+        return feedController
+    }
+    
+    private static func adaptFeedCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
+        return { [weak controller] feed in
+            controller?.tableModel = feed.map { model in
+                FeedImageCellController(viewModel: FeedImageViewModel(model: model, imageLoader: loader, imageTransformer: UIImage.init))
+            }
+        }
+    }
+}
