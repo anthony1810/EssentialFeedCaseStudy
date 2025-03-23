15adca11089acfae195481a895c519ee1360d147
Anthony Tran <qquang269@gmail.com>
2025-03-23 09:10:01 +0700

    replace `RefeshControl` from code with storyboard initilization

--- EssentialFeed/EssentialFeediOS/Feed UI/Composer/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Composer/FeedUIComposer.swift
@@ -7,12 +7,13 @@ public final class FeedUIComposer {
     public static func feedComposedWith(feedLoader: FeedLoader, imageLoader: FeedImageDataLoader) -> FeedViewController {
         
         let feedLoaderPresenterAdapter = FeedLoaderPresenterAdapter(loader: feedLoader)
-       
-        let refreshController = FeedRefreshViewController(delegate: feedLoaderPresenterAdapter)
+        
         let bundle = Bundle(for: FeedViewController.self)
         let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
         let feedController = storyboard.instantiateInitialViewController() as! FeedViewController
-        feedController.refreshController = refreshController
+       
+        let refreshController = feedController.refreshController!
+        refreshController.delegate = feedLoaderPresenterAdapter
         
         let feedPresenter = FeedPresenter(
             loadingView: WeakRefVirtualProxy(object: refreshController),
--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -4,29 +4,19 @@ public protocol FeedRefreshViewControllerDelegate {
 }
 
 public final class FeedRefreshViewController: NSObject, LoadingView {
-    public lazy var view: UIRefreshControl = loadView()
+    @IBOutlet public var view: UIRefreshControl?
     
-    private let delegate: FeedRefreshViewControllerDelegate
+    public var delegate: FeedRefreshViewControllerDelegate?
     
-    init(delegate: FeedRefreshViewControllerDelegate) {
-        self.delegate = delegate
-    }
-    
-    @objc func refresh() {
-        delegate.didRequestFeedRefresh()
+    @IBAction func refresh() {
+        delegate?.didRequestFeedRefresh()
     }
     
     func display(viewModel: LoadingViewModel) {
         if viewModel.isLoading {
-            self.view.beginRefreshing()
+            self.view?.beginRefreshing()
         } else {
-            self.view.endRefreshing()
+            self.view?.endRefreshing()
         }
-    }
-    
-    private func loadView() -> UIRefreshControl{
-        let view = UIRefreshControl()
-        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
-        return view
     }
 }
