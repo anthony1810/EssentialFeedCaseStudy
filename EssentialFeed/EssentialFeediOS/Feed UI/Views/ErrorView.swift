ccf4a882f926e3aa8d3730ddf52ac3e5c502b256
Anthony Tran <qquang269@gmail.com>
2025-03-29 13:37:52 +0700

    show and hide error message animated

--- EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -19,4 +19,34 @@ public final class ErrorView: UIView {
         
         label.text = nil
     }
+    
+    private var isVisible: Bool {
+        alpha > 0
+    }
+    
+    private func setMessageAnimated(_ message: String?) {
+        if let message {
+            showAnimated(message)
+        } else {
+            hideMessageAnimated()
+        }
+    }
+    
+    private func showAnimated(_ message: String) {
+        label.text = message
+        
+        UIView.animate(withDuration: 0.3) {
+            self.alpha = 1
+        }
+    }
+    
+    private func hideMessageAnimated() {
+        UIView.animate(
+            withDuration: 0.25,
+            animations: { self.alpha = 0},
+            completion: { completed in
+                if !completed { return }
+                self.label.text = nil
+            })
+    }
 }
