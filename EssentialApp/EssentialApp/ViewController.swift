baf4c937c1b626c097602c4b5d000f7df6d00e62
Anthony Tran <qquang269@gmail.com>
2025-07-06 18:43:36 +0700

    Add empty project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Anthony on 6/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
