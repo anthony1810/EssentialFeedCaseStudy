5fd7dce512a29e0e8dd02881521cf7042e3a0570
Anthony Tran <qquang269@gmail.com>
2025-07-23 11:43:51 +0700

    Add UI Acceptance tests

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialAppUIAcceptanceTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Anthony on 23/7/25.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTests: XCTestCase {
+
+   
+}
