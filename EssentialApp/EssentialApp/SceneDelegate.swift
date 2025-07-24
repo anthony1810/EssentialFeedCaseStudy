bab6a7b2abcfe226cfc6cba366a1936fdcedf456
Anthony Tran <qquang269@gmail.com>
2025-07-24 10:15:25 +0700

    allow SceneDelegate to inject custom httpClient for RemoteFeedLoader and store for LocalFeedStore & LocalFeedImageDataStore

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -13,12 +13,29 @@ import CoreData
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    private lazy var httpClient: HTTPClient = {
+        let session = URLSession(configuration: .ephemeral)
+        let client = URLSessionHTTPClient(session: session)
+        return client
+    }()
+    
+    private lazy var store: FeedStore & FeedImageDataStore = {
+        let storeURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("EssentialFeed.sqlite")
+        let store = try! CoreDataFeedStore(storeURL: storeURL)
+        
+        return store
+    }()
+    
+    convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
+        self.init()
+        
+        self.httpClient = httpClient
+        self.store = store
+    }
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
         configureWindow()
@@ -26,12 +43,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func configureWindow() {
         let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
-        let session = URLSession(configuration: .ephemeral)
-        let client = URLSessionHTTPClient(session: session)
-        let remoteFeedLoader = RemoteFeedLoader(url: url, client: client)
         
-        let storeURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("EssentialFeed.sqlite")
-        let store = try! CoreDataFeedStore(storeURL: storeURL)
+        let remoteFeedLoader = RemoteFeedLoader(url: url, client: httpClient)
         let localFeedLoader = LocalFeedLoader(store: store, currentDate: Date.init)
         
         // Feed
@@ -44,7 +57,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         )
         
         // Feed Image
-        let remoteFeedImageLoader = RemoteFeedImageDataLoader(client: client)
+        let remoteFeedImageLoader = RemoteFeedImageDataLoader(client: httpClient)
         let localFeedImageLoader = LocalFeedImageDataLoader(store: store)
         let feedImageLoader = FeedImageLoaderWithFallbackComposite(
             primary: FeedImageLoaderCacheDecorater(
@@ -61,35 +74,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window?.rootViewController = UINavigationController(rootViewController: feedVC)
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 
