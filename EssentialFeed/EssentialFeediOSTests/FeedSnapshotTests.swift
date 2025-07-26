4e91acafc93df876abda153f3c4ec8bb029a4f8b
Anthony Tran <qquang269@gmail.com>
2025-07-26 15:51:20 +0700

    add dark mode support for snapshot tests

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -15,7 +15,8 @@ final class FeedSnapshotTests: XCTestCase {
         
         sut.display(emptyFeed())
         
-        assert(snapshot: sut.snapshot(), named: "EMPTY_FEED")
+        record(snapshot: sut.snapshot(for: .iphone8(style: .light)), named: "EMPTY_FEED")
+        record(snapshot: sut.snapshot(for: .iphone8(style: .dark)), named: "EMPTY_FEED_DARK")
     }
     
     func test_renderFeed_whenNotEmpty() throws {
@@ -23,7 +24,8 @@ final class FeedSnapshotTests: XCTestCase {
         
         sut.display(nonEmptyFeed())
         
-        assert(snapshot: sut.snapshot(), named: "NOT_EMPTY_FEED")
+        record(snapshot: sut.snapshot(for: .iphone8(style: .light)), named: "NOT_EMPTY_FEED")
+        record(snapshot: sut.snapshot(for: .iphone8(style: .dark)), named: "NOT_EMPTY_FEED_DARK")
     }
     
     func test_renderFeed_whenThereIsError() throws {
@@ -31,7 +33,8 @@ final class FeedSnapshotTests: XCTestCase {
         
         sut.display(errorOccured("There is an error \n please try again later"))
         
-        assert(snapshot: sut.snapshot(), named: "ERROR_OCCURED")
+        record(snapshot: sut.snapshot(for: .iphone8(style: .light)), named: "ERROR_OCCURED")
+        record(snapshot: sut.snapshot(for: .iphone8(style: .dark)), named: "ERROR_OCCURED_DARK")
     }
     
     func test_renderFeed_whenImageFailedToLoad() throws {
@@ -39,7 +42,8 @@ final class FeedSnapshotTests: XCTestCase {
         
         sut.display(feedWithFailedImageLoading())
         
-        assert(snapshot: sut.snapshot(), named: "IMAGE_FAILED_TO_LOAD")
+        record(snapshot: sut.snapshot(for: .iphone8(style: .light)), named: "IMAGE_FAILED_TO_LOAD")
+        record(snapshot: sut.snapshot(for: .iphone8(style: .dark)), named: "IMAGE_FAILED_TO_LOAD_DARK")
     }
     
     // MARK: - Helpers
@@ -158,6 +162,69 @@ final class FeedSnapshotTests: XCTestCase {
     }
 }
 
+struct SnapshotConfiguration {
+    let size: CGSize
+    let safeAreaInsets: UIEdgeInsets
+    let layoutMargins: UIEdgeInsets
+    let traitCollection: UITraitCollection
+    
+    static func iphone8(style: UIUserInterfaceStyle) -> SnapshotConfiguration {
+        SnapshotConfiguration(
+            size: CGSize(width: 375, height: 667),
+            safeAreaInsets: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0),
+            layoutMargins: UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16),
+            traitCollection: UITraitCollection(mutations: { mutableTraits in
+                mutableTraits.forceTouchCapability = .available
+                mutableTraits.layoutDirection = .leftToRight
+                mutableTraits.userInterfaceIdiom = .phone
+                mutableTraits.horizontalSizeClass = .compact
+                mutableTraits.verticalSizeClass = .regular
+                mutableTraits.displayScale = 2
+                mutableTraits.displayGamut = .P3
+                mutableTraits.userInterfaceStyle = style
+            })
+        )
+    }
+}
+
+private final class SnapshotWindow: UIWindow {
+    private var configuration: SnapshotConfiguration = .iphone8(style: .light)
+    
+    convenience init(configuration: SnapshotConfiguration, root: UIViewController) {
+        self.init(frame: CGRect(origin: .zero, size: configuration.size))
+        
+        self.configuration = configuration
+        self.layoutMargins = configuration.layoutMargins
+        self.rootViewController = root
+        self.isHidden = false
+        root.view.layoutMargins = configuration.layoutMargins
+    }
+    
+    override var safeAreaInsets: UIEdgeInsets {
+        configuration.safeAreaInsets
+    }
+    
+    override var traitCollection: UITraitCollection {
+        UITraitCollection { mutableTraits in
+            mutableTraits.forceTouchCapability = configuration.traitCollection.forceTouchCapability
+            mutableTraits.layoutDirection = configuration.traitCollection.layoutDirection
+            mutableTraits.userInterfaceIdiom = configuration.traitCollection.userInterfaceIdiom
+            mutableTraits.horizontalSizeClass = configuration.traitCollection.horizontalSizeClass
+            mutableTraits.verticalSizeClass = configuration.traitCollection.verticalSizeClass
+            mutableTraits.displayScale = configuration.traitCollection.displayScale
+            mutableTraits.displayGamut = configuration.traitCollection.displayGamut
+            mutableTraits.userInterfaceStyle = configuration.traitCollection.userInterfaceStyle
+        }
+    }
+    
+    func snapshot() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds, format: .init(for: traitCollection))
+        return renderer.image { action in
+            layer.render(in: action.cgContext)
+        }
+    }
+}
+
 private class ImageStub: FeedImageCellControllerDelegate {
     weak var controller: FeedImageCellController?
     private let viewModel: FeedImageViewModel<UIImage>
@@ -192,11 +259,8 @@ private extension FeedViewController {
 }
 
 extension UIViewController {
-    func snapshot() -> UIImage {
-        let renderer = UIGraphicsImageRenderer(bounds: view.bounds)
-        return renderer.image { action in
-            view.layer.render(in: action.cgContext)
-        }
+    func snapshot(for configuration: SnapshotConfiguration) -> UIImage {
+        SnapshotWindow(configuration: configuration, root: self).snapshot()
     }
 }
 
