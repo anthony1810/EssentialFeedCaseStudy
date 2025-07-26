5b3fe4049e8398d9e8d8ab2921fb41fc2545cdc6
Anthony Tran <qquang269@gmail.com>
2025-07-26 14:58:02 +0700

    record snapshot of error message

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -34,6 +34,14 @@ final class FeedSnapshotTests: XCTestCase {
         record(snapshot: sut.snapshot(), named: "ERROR_OCCURED")
     }
     
+    func test_renderFeed_whenImageFailedToLoad() throws {
+        let sut = makeSUT()
+        
+        sut.display(feedWithFailedImageLoading())
+        
+        record(snapshot: sut.snapshot(), named: "IMAGE_FAILED_TO_LOAD")
+    }
+    
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
@@ -52,7 +60,7 @@ final class FeedSnapshotTests: XCTestCase {
     }
     
     private func nonEmptyFeed() -> [ImageStub] {
-        return [
+        [
             ImageStub(
                 description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
                 location: "East Side Gallery\nMemorial in Berlin, Germany",
@@ -61,7 +69,23 @@ final class FeedSnapshotTests: XCTestCase {
             ImageStub(
                 description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
                 location: "Garth Pier",
-                image: UIImage.make(withColor: .cyan))
+                image: UIImage.make(withColor: .cyan)
+            )
+        ]
+    }
+    
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        [
+            ImageStub(
+                description: nil,
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: nil
+            ),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: nil
+            )
         ]
     }
     
