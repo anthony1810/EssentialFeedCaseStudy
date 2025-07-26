fc55d25031f09678000915458a8764df6776e4ab
Anthony Tran <qquang269@gmail.com>
2025-07-26 10:29:31 +0700

    snapshot feed view controlelr with content

--- EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -6,7 +6,7 @@
 //
 import Foundation
 import XCTest
-import EssentialFeed
+@testable import EssentialFeed
 import EssentialFeediOS
 
 final class FeedSnapshotTests: XCTestCase {
@@ -16,6 +16,14 @@ final class FeedSnapshotTests: XCTestCase {
         sut.display(emptyFeed())
         
         record(snapshot: sut.snapshot(), named: "EMPTY_FEED")
+    }
+    
+    func test_renderFeed_whenNotEmpty() throws {
+        let sut = makeSUT()
+        
+        sut.display(nonEmptyFeed())
+        
+        record(snapshot: sut.snapshot(), named: "NOT_EMPTY_FEED")
     }
     
     // MARK: - Helpers
@@ -29,6 +37,20 @@ final class FeedSnapshotTests: XCTestCase {
     
     private func emptyFeed() -> [FeedImageCellController] {
         []
+    }
+    
+    private func nonEmptyFeed() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .systemPink)
+            ),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .cyan))
+        ]
     }
     
     private func record(
@@ -57,6 +79,39 @@ final class FeedSnapshotTests: XCTestCase {
         } catch {
             XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
         }
+    }
+}
+
+private class ImageStub: FeedImageCellControllerDelegate {
+    weak var controller: FeedImageCellController?
+    private let viewModel: FeedImageViewModel<UIImage>
+    
+    init(description: String?, location: String?, image: UIImage?) {
+        self.viewModel = FeedImageViewModel(
+            location: location,
+            description: description,
+            image: image,
+            isLoading: false,
+            shouldRetry: image == nil
+        )
+    }
+    
+    func didRequestImage() {
+        controller?.display(viewModel: viewModel)
+    }
+    
+    func didCancelImageRequest() {}
+}
+
+private extension FeedViewController {
+    func display(_ stubs: [ImageStub]) {
+        self.display(
+            stubs.map { stub in
+                let controller = FeedImageCellController(delegate: stub)
+                stub.controller = controller
+                return controller
+            }
+        )
     }
 }
 
