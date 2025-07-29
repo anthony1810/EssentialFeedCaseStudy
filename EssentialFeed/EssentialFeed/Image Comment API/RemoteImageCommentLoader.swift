7acf3bb60ad620f5ea12ecc16b2f5ad6f11231f1
Anthony Tran <qquang269@gmail.com>
2025-07-29 10:21:44 +0700

    Replace RemoteImageCommantLoader with RemoteLoader generic

--- EssentialFeed/EssentialFeed/Image Comment API/RemoteImageCommentLoader.swift
+++ EssentialFeed/EssentialFeed/Image Comment API/RemoteImageCommentLoader.swift
@@ -5,43 +5,10 @@
 //  Created by Anthony on 28/7/25.
 //
 import Foundation
+public typealias RemoteImageCommentLoader = RemoteLoader<[ImageComment]>
 
-public final class RemoteImageCommentLoader {
-    
-    public typealias Result = Swift.Result<[ImageComment], Swift.Error>
-    
-    let url: URL
-    let client: HTTPClient
-    
-    public enum Error: Swift.Error {
-        case connectivity
-        case invalidData
-    }
-    
-    public init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url, completion: { [weak self] result in
-            guard self != nil else { return }
-            
-            switch result {
-            case let .success((data, res)):
-                completion(Self.map(data: data, res: res))
-            case .failure:
-                completion(.failure(Error.connectivity))
-            }
-        })
-    }
-    
-    private static func map(data: Data, res: HTTPURLResponse) -> Result {
-        do {
-            let items = try ImageCommentMapper.map(data, res: res)
-            return .success(items)
-        } catch {
-            return .failure(Error.invalidData)
-        }
+public extension RemoteImageCommentLoader {
+    convenience init(url: URL, client: HTTPClient) {
+        self.init(url: url, client: client, mapper: ImageCommentMapper.map)
     }
 }
--- EssentialFeed/EssentialFeedTests/Image Comment/LoadImageCommentFromRemoteUseCase.swift
+++ EssentialFeed/EssentialFeedTests/Image Comment/LoadImageCommentFromRemoteUseCase.swift
@@ -10,40 +10,6 @@ import EssentialFeed
 
 class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
 
-    func test_init_doesNotRequestDataFromURL() {
-        let (_, client) = makeSUT()
-        
-        XCTAssertTrue(client.requestedURLs.isEmpty)
-    }
-    
-    func test_load_requestsDataFromURL() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.load { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url])
-    }
-    
-    func test_loadTwice_requestsDataFromURLTwice() {
-        let url = URL(string: "https://a-given-url.com")!
-        let (sut, client) = makeSUT(url: url)
-        
-        sut.load { _ in }
-        sut.load { _ in }
-        
-        XCTAssertEqual(client.requestedURLs, [url, url])
-    }
-    
-    func test_load_deliversErrorOnClientError() {
-        let (sut, client) = makeSUT()
-        
-        expect(sut, toCompleteWith: failure(.connectivity), when: {
-            let clientError = NSError(domain: "Test", code: 0)
-            client.complete(withError: clientError)
-        })
-    }
-    
     func test_load_deliversErrorOnNon2xxHTTPResponse() {
         let (sut, client) = makeSUT()
         
@@ -111,20 +77,6 @@ class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
         }
     }
     
-    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
-        let url = URL(string: "http://any-url.com")!
-        let client = HTTPClientSpy()
-        var sut: RemoteImageCommentLoader? = RemoteImageCommentLoader(url: url, client: client)
-        
-        var capturedResults = [RemoteImageCommentLoader.Result]()
-        sut?.load { capturedResults.append($0) }
-
-        sut = nil
-        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
-        
-        XCTAssertTrue(capturedResults.isEmpty)
-    }
-    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!, file: StaticString = #file, line: UInt = #line) -> (sut: RemoteImageCommentLoader, client: HTTPClientSpy) {
@@ -167,7 +119,7 @@ class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
             case let (.success(receivedItems), .success(expectedItems)):
                 XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
                 
-            case let (.failure(receivedError as RemoteImageCommentLoader.Error), .failure(expectedError as RemoteImageCommentLoader.Error)):
+            case let (.failure(receivedError), .failure(expectedError)):
                 XCTAssertEqual(receivedError, expectedError, file: file, line: line)
                 
             default:
