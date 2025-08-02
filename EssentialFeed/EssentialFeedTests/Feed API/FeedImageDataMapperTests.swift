96d48a2277cce12d369095d758465b4c1c508fd0
Anthony Tran <qquang269@gmail.com>
2025-08-02 09:54:35 +0700

    replace FeedLoader.Result with LocalFeedLoader.LoadResult for Feed Cache Test

--- EssentialFeed/EssentialFeedTests/Feed API/FeedImageDataMapperTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedImageDataMapperTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class LoadFeedImageFromRemoteUseCaseTests: XCTestCase {
+class FeedImageDataMapperTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         let (_, client) = makeSUT(url: anyURL())
