1237957aa58d5e17cf1ec3a947eaa55fff5c7a6c
Anthony Tran <qquang269@gmail.com>
2025-09-05 08:54:40 +0700

    set feed endpoint limit to 10

--- EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
+++ EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
@@ -13,7 +13,14 @@ public enum FeedEndpoint {
     public func url(baseURL: URL) -> URL {
         switch self {
         case .get:
-            return baseURL.appendingPathComponent("/v1/feed")
+            var component = URLComponents()
+            component.scheme = baseURL.scheme
+            component.host = baseURL.host
+            component.path = baseURL.path() + "/v1/feed"
+            component.queryItems = [
+                URLQueryItem(name: "limit", value: "10")
+            ]
+            return component.url!
         }
     }
 }
--- EssentialFeed/EssentialFeedTests/Feed API/FeedEndpointTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedEndpointTests.swift
@@ -12,10 +12,13 @@ import EssentialFeed
 final class FeedEndpointTests: XCTestCase {
     func test_feed_endpointURL() {
         let baseURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed")!
-        let expected = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+        let expected = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed?limit=10")!
         
         let actual = FeedEndpoint.get.url(baseURL: baseURL)
         
-        XCTAssertEqual(actual, expected)
+        XCTAssertEqual(actual.scheme, expected.scheme)
+        XCTAssertEqual(actual.host, expected.host)
+        XCTAssertEqual(actual.path, expected.path)
+        XCTAssertEqual(actual.query(percentEncoded: true), expected.query(percentEncoded: true))
     }
 }
